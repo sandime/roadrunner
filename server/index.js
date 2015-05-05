0ce105033f7a3ac9f82eded25028afdfbf7187c6
sherri <sherri@sherricruz.com>
2015-05-05 16:05:18 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by SHERRI on 5/5/15.
+ */
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
